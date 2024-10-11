ba1fcbd2add39dcd602c40251a03130291247ace
sonneart <160793934+sonneart@users.noreply.github.com>
2024-10-11 23:59:39 +0200

    added rolls

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,19 @@ Hooks.on("init", function () {
   );
 });
 
-Hooks.on("ready", function () {
-  console.log(
-    "This code runs once core initialization is ready and game data is available."
-  );
+Hooks.on("ready", async function () {
+  // Construct the Roll instance
+  let r = new Roll("2d20kh + @prof + @strMod", { prof: 2, strMod: 4 });
+
+  // The parsed terms of the roll formula
+  console.log(r.terms); // [Die, OperatorTerm, NumericTerm, OperatorTerm, NumericTerm]
+
+  // Execute the roll
+  await r.evaluate();
+
+  // The resulting equation after it was rolled
+  console.log(r.result); // 16 + 2 + 4
+
+  // The total resulting from the roll
+  console.log(r.total); // 22
 });
